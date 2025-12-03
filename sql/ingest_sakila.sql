870c8343b270cefb5da48a3c96e3816137656531
ghaliaghazal <ghalia.ghazal2009@gmail.com>
2025-12-03 15:45:35 +0100

    uppdate

--- sql/ingest_sakila.sql
+++ sql/ingest_sakila.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS sakila.film (
     film_id INT PRIMARY KEY,
-    title VARCHAR() NOT NULL,
-    release_year YEAR NOT NULL
+    title VARCHAR NOT NULL,
+    release_year INT NOT NULL,
     language_id INT NOT NULL,
     rental_rate INT NOT NULL,
     length INT NOT NULL,
     replacement_cost INT NOT NULL,
-    rating VARCHAR() NOT NULL,
-    special_features VARCHAR() NOT NULL
+    rating VARCHAR NOT NULL,
+    special_features TEXT NOT NULL
     );
 
